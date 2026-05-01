93b85cf0f83f78ee048d88423e20cffd6b45b3c4
HumeyraErtas <78751869+HumeyraErtas@users.noreply.github.com>
2026-05-01 15:12:50 +0300

    değişiklik

---
+++ entity/User.java
@@ -0,0 +1,12 @@
+@Entity
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+}
+
